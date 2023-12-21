1f2bcd812e8aaf145721ee041b9643b53ca822ca
Marta109 <marta.hayrapetyan.a@gmail.com>
2023-12-22 03:44:02 +0400

    feat: add answers

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const {NotImplementedError} = require("../extensions/index.js");
 
 /**
  * In the popular Minesweeper game you have a board with some mines and those cells
@@ -23,11 +23,38 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+function minesweeper(matrix) {
+  const result = [];
+  let bool = false;
+  for (let i = 0; i < matrix.length; i++) {
+    result.push([]);
+    for (let j = 0; j < matrix[i].length; j++) {
+      result[i][j] = 0;
+      if (matrix[i][j] === true) {
+        bool = true;
+        if (j === 0) {
+          result[i][j] = 1;
+          result[i][j + 1] = 2;
+          j++;
+        } else {
+          result[i][j] = 1;
+          result[i][j - 1] = 2;
+        }
+      } else if (bool) {
+        result[i][j] = 1;
+      }
+    }
+  }
+  return result;
 }
 
+minesweeper([
+  [true, false, false],
+  [false, true, false],
+  [false, false, false],
+]);
+
 module.exports = {
-  minesweeper
+  minesweeper,
 };
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -19,16 +19,91 @@ const {NotImplementedError} = require("../extensions/index.js");
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
  *
  */
+
+// const matrixVigenere = {};
+
+// function createMatrixVigenere() {
+//   let alphabet = "abcdefghijklmnopqrstuvwxyz".toUpperCase();
+//   for (let i = 0; i < alphabet.length; i++) {
+//     if (matrixVigenere[alphabet[i]] === undefined)
+//       if (i === 0) {
+//         matrixVigenere[alphabet[i]] = alphabet;
+//       } else {
+//         matrixVigenere[alphabet[i]] =
+//           alphabet.slice(i, alphabet.length) + alphabet.slice(0, i);
+//       }
+//   }
+// }
+
+// createMatrixVigenere();
+
 class VigenereCipheringMachine {
-  encrypt(arg) {
-    throw new NotImplementedError('Not implemented');
-    // if (!arg) throw new Error("Incorrect arguments!");
-    // remove line with error and write your code here
+  constructor(isDirect = true) {
+    this.isDirect = isDirect;
+    this.alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   }
-  decrypt(arg) {
-    throw new NotImplementedError('Not implemented');
-    // if (!arg) throw new Error("Incorrect arguments!");
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+    if (!message || !key) throw new Error("Incorrect arguments!");
+
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+
+    let messageStr = message.replace(/[^A-Z]/g, "");
+    key = this.completeKey(key, messageStr);
+    let j = 0;
+    let result = "";
+
+    for (let i = 0; i < message.length; i++) {
+      if (/[^A-Z]/.test(message[i])) {
+        result += message[i];
+      } else {
+        let indexMess = this.alphabet.indexOf(messageStr[j]);
+        let indexKey = this.alphabet.indexOf(key[j]);
+        let indexResult = (indexMess + indexKey) % 26;
+        result += this.alphabet[indexResult];
+        j++;
+      }
+    }
+    return this.isDirect ? result : result.split("").reverse().join("");
+  }
+
+  
+  decrypt(encryptedMessage, key) {
+    if (!encryptedMessage || !key) throw new Error("Incorrect arguments!");
+
+    encryptedMessage = encryptedMessage.toUpperCase();
+    key = key.toUpperCase();
+
+    let messageStr = encryptedMessage.replace(/[^A-Z]/g, "");
+    key = this.completeKey(key, messageStr);
+    let j = 0;
+    let result = "";
+
+    for (let i = 0; i < encryptedMessage.length; i++) {
+      if (/[^A-Z]/.test(encryptedMessage[i])) {
+        result += encryptedMessage[i];
+      } else {
+        let indexMess = this.alphabet.indexOf(messageStr[j]);
+        let indexKey = this.alphabet.indexOf(key[j]);
+        let indexResult = (indexMess - indexKey) % 26;
+        if (indexResult < 0) {
+          indexResult = (indexResult + 26) % 26;
+        }
+        result += this.alphabet[indexResult];
+        j++;
+      }
+    }
+    return this.isDirect ? result : result.split("").reverse().join("");
+  }
+
+  completeKey(key, message) {
+    let j = 0;
+    while (key.length < message.length) {
+      key += key[j];
+      j++;
+    }
+    return key;
   }
 }
 
